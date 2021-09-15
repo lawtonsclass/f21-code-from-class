1b5dc1058643272df3f89d177e2beae407b2baa5
Lawton Nichols <lawtonnichols@gmail.com>
2021-09-14 20:23:07 -0700

    .

---
+++ csci40/lec11/review_session/infiniteloop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // while (true) {
+    // cout << "hello\n";
+  // }
+  
+  // int i = 1;
+  // while (i <= 10) {
+    // cout << i << endl;
+    // i++; // infinite loop if you forget this
+  // }
+  
+  // Do 4 times:
+  //   print a single line of 4 stars
+
+  for (int i = 0; i > -4; i++) { // do 4 times
+    // print 4 *s
+    for (int j = 0; j < 4; j++) {
+      cout << "*";
+    }
+    // print a \n
+    cout << "\n";
+  }
+
+
+  return 0;
+}
