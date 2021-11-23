e3c1ba3a2957b291a5fb1d2b9884f1a0a76cd74d
Lawton Nichols <lawtonnichols@gmail.com>
2021-11-22 18:22:16 -0800

    .

---
+++ csci45/lec28/scratch.s
@@ -0,0 +1,10 @@
+.global _start // _start is a global label
+_start: // this marks the start of the _start section
+  add r0, r0, #0 // nop -- no operation -- this instruction does nothing
+
+  @ this is also a comment!
+  /* this too */
+  mov r0, #42  // move the #0 into register R0
+  mov r7, #1  // move the #1 into the resiter R7
+  // 1 is the code for the "exit" system call
+  swi 0  // please execute the system call I just set up!
